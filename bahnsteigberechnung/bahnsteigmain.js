d632b1b0152539af8d33d80751cf5e29269522ee
mohrly123 <168989632+mohrly123@users.noreply.github.com>
2024-11-09 15:20:24 +0100

    0.2

--- bahnsteigberechnung/bahnsteigmain.js
+++ bahnsteigberechnung/bahnsteigmain.js
@@ -1,16 +1,13 @@
-
-const übernommeneDaten = JSON.parse(localStorage.getItem("übergebeneDaten"));
-
-console.log(übernommeneDaten);
-
-if (übernommeneDaten){
-    console.log("Daten erfolgreich geladen")
-}
-else{
-    console.log("ERROR/ Daten nicht gefunden")
-}
-let abstand = übernommeneDaten.abstand;
-let hoehe = übernommeneDaten.hoehe;
-let ordnungsnummer = übernommeneDaten.ordnungsnummer;
-
-console.log(`Abstand: ${abstand} Höhe: ${hoehe} ordnungsnummer: ${ordnungsnummer}`)
+// übernehmen der Daten aus dem lokalen Speicher und in der Variable "abgeholteDaten speichern"
+const abgeholteDaten = JSON.parse(localStorage.getItem("übergebeneDaten"));
+// Umwandeln der Daten in die entsprechenden Werte
+let abstand = abgeholteDaten.abstand;
+let minAbstand = abgeholteDaten.minAbstand;
+let maxAbstand = abgeholteDaten.maxAbstand;
+let hoehe = abgeholteDaten.hoehe;
+let minHoehe = abgeholteDaten.minHoehe;
+let maxHoehe = abgeholteDaten.maxHoehe;
+// Ausgabe der Daten in der Konsole
+console.log(`Erfolgreich abgeholt. Daten: ${abstand}, ${minAbstand}, ${maxAbstand}, ${hoehe}, ${minHoehe}, ${maxHoehe}`);
+// Setzten des Titels der Seite auf den Entsprechenden Bahnsteig
+document.getElementById("titlebahnsteig").textContent = `Bahnsteigberechnung ${hoehe} mm`;
--- main.js
+++ main.js
@@ -1,22 +1,27 @@
-
-
-function openBahnsteig(abstand, hoehe, ordnungsnummer) {
+// Funktion um die Zahlen des Buttons zu nehmen
+function openBahnsteig(abstand,minAbstand,maxabstand,hoehe,minHoehe,maxHoehe){
     this.abstand = abstand;
+    this.minAbstand = minAbstand;
+    this.maxabstand = maxabstand;
     this.hoehe = hoehe;
-    this.ordnungsnummer = ordnungsnummer;
-    saveInLocalStorage(abstand,hoehe,ordnungsnummer);
+    this.minHoehe = minHoehe;
+    this.maxHoehe = maxHoehe;
+    saveToLocalStorage(abstand,minAbstand,maxabstand,hoehe,minAbstand,maxHoehe);
     window.location.href = "/bahnsteigberechnung/bahnsteig.html"
-
 }
 
-function saveInLocalStorage(abstand, hoehe, ordnungsnummer){
+// Funktion um die Zahlen in den local Storage zu packen
+function saveToLocalStorage(abstand,minAbstand,maxAbstand,hoehe,minHoehe,maxHoehe){
+    // Clearen des Storage
     localStorage.clear();
-    localStorage.setItem("übergebeneDaten", JSON.stringify({abstand, hoehe,ordnungsnummer}));
-    if (localStorage.getItem("übergebeneDaten")){
-        console.log("Daten gespeichert");
-
-    }
-    else{
-        console.log("ERROR DATEN NICHT GESPEICHERT")
+    // Daten in den lokalen Speicher stecken
+    localStorage.setItem("übergebeneDaten", JSON.stringify({abstand,minAbstand,maxAbstand,hoehe,minHoehe,maxHoehe}));
+    // Überprüfen ob es "übergebeneDaten" im lokalen Speicher gibt
+    if(localStorage.getItem("übergebeneDaten")){
+        // Wenn Ja dann ausgabe der Daten in der Konsole
+        console.log(`Erfolgreich gespeichert. Daten: ${abstand}, ${minAbstand}, ${maxAbstand}, ${hoehe}, ${minHoehe}, ${maxHoehe}`);
+        // Wenn nicht dann ausgabe eines Fehlers in der Konsole
+    }else{
+        console.log("Fehler! Daten nicht im lokal Storage gespeichert");
     }
 }
